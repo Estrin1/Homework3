c5c85b0e56f2555538114bb8594a55e24b5a741f
Evgeniy <estrin.j@yandex.ru>
2022-11-27 11:33:47 +0300

    new save2

---
+++ z4 dop/Program.cs
@@ -0,0 +1,12 @@
+﻿//Дано трехзначное число определить кратна ли 3 сумма его чисел
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int i=0,j=0,q=0;
+i = number / 100;
+Console.WriteLine(i);
+j = number % 10; 
+Console.WriteLine(j);
+q = (number - i*100) / 10;
+Console.WriteLine(q);
+if ((i+j+q) % 3 ==0 ) Console.WriteLine("Сумма чисел кратна 3");
+else Console.WriteLine("Сумма чисел не кратна 3");
---
+++ z5 dop/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определить есть ли в 3х значном числе цифры 4 или 7
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int i=0,j=0,q=0;
+i = number / 100;
+Console.WriteLine(i);
+j = number % 10; 
+Console.WriteLine(j);
+q = (number - i*100) / 10;
+Console.WriteLine(q);
+if ( i  == 4 | i == 7) Console.WriteLine("Искомые цифры присутствуют в числе");
+else if ( j  == 4 | j == 7) Console.WriteLine("Искомые цифры присутствуют в числе");
+else if ( q  == 4 | q == 7) Console.WriteLine("Искомые цифры присутствуют в числе");
+else Console.WriteLine("Искомые цифры отсутствуют в числе");
